f92c61dbeee174f3b74e735e655cedbbdfae65b8
gingerfatman <78505781+gingerfatman@users.noreply.github.com>
2021-02-08 23:11:47 -0500

    simplified loop

--- vowels_loop.py
+++ vowels_loop.py
@@ -3,21 +3,12 @@ s = 'Today was a lovely day. I took a trip to the beach.'
 num = 0
 
 for letter in s: 
-    if letter == 'a':
+    if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
         num = num + 1
-    elif letter == 'e':
-        num = num + 1
-    elif letter == 'i':
-        num = num + 1
-    elif letter == 'o':
-        num = num + 1
-    elif letter == 'u':
-        num = num + 1
-    else: 
-        continue
-
+   
 msg = 'This sentence has {} vowels.'
 print(msg.format(num))
+
 
 
 
